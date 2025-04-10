978b97b5057544214d3cff95f3db1b6903934c26
priymaurya207 <priyamaurya207@gmail.com>
2025-04-10 12:15:45 +0530

    User Class Component implemented

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,10 @@
+import User from "./User";
 const About =()=>{
     return (
         <div className="about">
             <h1>About Us</h1>
             <h2>This is about us</h2>
+            <User name={"Priya Maurya"} location = {"Delhi"}/>
         </div>
     )
 }
